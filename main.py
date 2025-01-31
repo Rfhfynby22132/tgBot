7380d95116d804a8c8b56375c443e1db7349e888
Rfhfynby22132 <k7499885@gmail.com>
2025-01-31 14:54:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import asyncio
 from aiogram import Bot,Dispatcher
 from app.handlers import router
-from app.database.medels import async_main
+
+
 
 async def main():
-    bot = Bot("7771898758:AAGi0cU60t1LhGyb8Cpm3uUke5Sfg6Q5vKw")
+    bot = Bot('7771898758:AAGi0cU60t1LhGyb8Cpm3uUke5Sfg6Q5vKw')
     dp = Dispatcher()
     dp.include_router(router)# добавляем router в Dispatcher чтобы он мог знать
     await dp.start_polling(bot)
-    await async_main()
+
+
 
 if __name__ == "__main__":
     try:
