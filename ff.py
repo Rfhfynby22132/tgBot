cb92183b26071666cbf2364b4f63d52ae66d8449
Rfhfynby22132 <k7499885@gmail.com>
2025-02-13 18:26:04 +0300

    Create ff.py

---
+++ ff.py
@@ -0,0 +1,12 @@
+TOKEN ='7771898758:AAGi0cU60t1LhGyb8Cpm3uUke5Sfg6Q5vKw'
+FOTO= 'https://sun9-4.userapi.com/impf/kjahbJi4RN1TcGUArb7HpYK13QCH0tgYXEnhfQ/0p_yT5bgYFQ.jpg?size=1818x606&quality=95&crop=0,45,1900,633&sign=05fbe6ac414e49950cbe4665e6e63b17&type=cover_group'
+FOUR = 'https://www.ozon.ru/product/krossovki-nike-tanjun-879761716/?at=08tYyvO6DcmA0jySmgV3WQTV68yOT2ZNyjVCOPKvAn&avtc=1&avte=4&avts=1738601173&keywords=кроссовки+мужские+nike+tanjun'
+
+
+
+
+
+
+
+
+
