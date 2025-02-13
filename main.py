9ee95bfcb741326af8b88f2eed63889f92400d34
Rfhfynby22132 <k7499885@gmail.com>
2025-02-13 18:24:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import asyncio
 from aiogram import Bot,Dispatcher
-from app.handlers import router
+from app_1.handlers import router
+from app.ff import TOKEN
+
 
 
 
 async def main():
-    bot = Bot(ТОКЕН ВАШ)
+    bot = Bot(TOKEN)
     dp = Dispatcher()
-    dp.include_router(router)# добавляем router в Dispatcher чтобы он мог знать
+    dp.include_router(router)
     await dp.start_polling(bot)
 
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
